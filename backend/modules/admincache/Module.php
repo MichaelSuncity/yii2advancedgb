9314d01f603a3519fa2e260c0d4c340deb734735
MichaelSuncity <mixail.pismarov@mail.ru>
2020-02-03 18:43:14 +0300

    кнопка очистки кэша

---
+++ backend/modules/admincache/Module.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace backend\modules\admincache;
+
+/**
+ * admincache module definition class
+ */
+class Module extends \yii\base\Module
+{
+    public $controllerNamespace = 'backend\modules\admincache\controllers';
+}
---
+++ backend/modules/admincache/views/default/index.php
@@ -0,0 +1,14 @@
+<?php
+
+use yii\helpers\Html;
+
+?>
+<h1>Очистка кэша</h1>
+<p><?=Html::a('Flush Cash', ['flush'], [
+    'class' => 'btn btn-success',
+    'data' => [
+        'confirm' => 'Are you sure you want to flush all cash?',
+        'method' => 'post',
+    ],
+]);?></p>
+

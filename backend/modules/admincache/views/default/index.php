a919e37713385bb9d6fa3a9a3074729a0b42c15f
MichaelSuncity <mixail.pismarov@mail.ru>
2020-02-03 18:51:42 +0300

    кнопка очистки кэша

--- backend/modules/admincache/views/default/index.php
+++ backend/modules/admincache/views/default/index.php
@@ -4,7 +4,7 @@ use yii\helpers\Html;
 
 ?>
 <h1>Очистка кэша</h1>
-<p><?=Html::a('Flush Cash', ['flush'], [
+<p><?= Html::a('Flush Cash', ['flush'], [
     'class' => 'btn btn-success',
     'data' => [
         'confirm' => 'Are you sure you want to flush all cash?',
